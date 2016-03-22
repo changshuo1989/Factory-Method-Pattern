10dadcd085f630fb2c7594e778078cb680a445b2
Changshuo <cgao@healthrecoverysolutions.com>
2016-03-22 15:33:57 -0400

    This is sample code for factory method pattern

---
+++ src/com/test/NYPizzaStore.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class NYPizzaStore extends PizzaStore {
+
+	@Override
+	protected Pizza createPizza(String item) {
+		if(item.equals("cheese")){
+				return new NYStyleCheesePizza();
+		}
+		else 
+			return null;
+	}
+}
+
+
